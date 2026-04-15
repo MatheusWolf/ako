af47f6f2a1eea9ac8e80d623bcbb1ac78c1001ea
MatheusWolf <Matheus-Wolf@hotmail.com>
2026-04-15 13:19:10 -0300

    fix(page): ajustando a frase impacto antes de enviar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O fracasso é o combustível do sucesso</h1>
+    return <h1>O fracasso é o combustível do sucesso, não tenha medo de errar</h1>
 }
 
 export default Home;
