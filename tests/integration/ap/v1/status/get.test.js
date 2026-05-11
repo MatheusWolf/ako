3d45994c3cdf14fb30bb5f1b08ccc1e5a39f47f4
MatheusWolf <Matheus-Wolf@hotmail.com>
2026-05-11 09:58:21 -0300

    finish `api/v1/status` endpoint

--- tests/integration/ap/v1/status/get.test.js
+++ tests/integration/ap/v1/status/get.test.js
@@ -3,4 +3,14 @@ const api_v1_status = "http://localhost:3000/api/v1/status";
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch(api_v1_status);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.opened_connection).toEqual(1);
 });
